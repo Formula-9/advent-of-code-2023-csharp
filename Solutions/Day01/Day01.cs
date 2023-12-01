60ccb52edb161e252675d1382779cf71c22efb8e
Maxime BARRAUD <maxime.barraud@protonmail.com>
2023-12-01 16:58:41 +0100

    D1: More concise and somehow faster...

--- Solutions/Day01/Day01.cs
+++ Solutions/Day01/Day01.cs
@@ -1,11 +1,15 @@
-using System.Collections.Immutable;
 using Formula9.AdventOfCode.Utils;
 
 namespace Formula9.AdventOfCode.Solutions2023.Day01;
 
 public class Day_01 : AdventOfCodeProblem
 {
-    public Day_01() : base(2023, 1) { }
+    private string[] SplitInput { get; init; }
+
+    public Day_01() : base(2023, 1) 
+    { 
+        SplitInput = Input.SplitByNewline();
+    }
 
     private static readonly IDictionary<string, string> Digits = new Dictionary<string, string>()
     {
@@ -18,17 +22,13 @@ public class Day_01 : AdventOfCodeProblem
         { "seven",  "s7n" },
         { "eight",  "e8t" },
         { "nine",   "n9e" }
-    }.ToImmutableDictionary();
+    };
 
-    private static int FindDigitPair(string s)
-    {
-        string altered  = s.ReplaceAll(Digits);
-        char first      = altered.First(char.IsDigit);
-        char last       = altered.Last(char.IsDigit);
-        return (first - '0') * 10 + (last - '0');
-    }
+    private static int FindDigitPair(string s) => SumFirstAndLastDigit(s.ReplaceAll(Digits));
 
-    public override ValueTask<string> Solve_1() => new(Input.SplitByNewline().Select(l => int.Parse(new char[] { l.First(char.IsDigit), l.Last(char.IsDigit) })).Sum().ToString());
+    private static int SumFirstAndLastDigit(string s) => (s.First(char.IsDigit) - '0') * 10 + (s.Last(char.IsDigit) - '0');
 
-    public override ValueTask<string> Solve_2() => new(Input.SplitByNewline().Select(FindDigitPair).Sum().ToString());
+    public override ValueTask<string> Solve_1() => new(SplitInput.Select(SumFirstAndLastDigit).Sum().ToString());
+
+    public override ValueTask<string> Solve_2() => new(SplitInput.Select(FindDigitPair).Sum().ToString());
 }
