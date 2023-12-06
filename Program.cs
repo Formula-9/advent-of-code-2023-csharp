94fb760c101c4a8e258b579b2f95d94eced17ef6
Maxime BARRAUD <maxime.barraud@protonmail.com>
2023-12-06 08:44:42 +0100

    Ultra-naive solution, will likely optimize it for fun later, both parts solved.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using AoCHelper;
-using Formula9.AdventOfCode.Solutions2023.Day05;
+using Formula9.AdventOfCode.Solutions2023.Day06;
+using Formula9.AdventOfCode.Solutions2023.Day07;
 
 namespace Formula9.AdventOfCode;
 
@@ -7,11 +8,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Solver.Solve<Day_05>(options => {
+        Solver.Solve<Day_07>(options => {
             options.ShowTotalElapsedTimePerDay = false;
             options.ShowOverallResults = false;
             options.ClearConsole = true;
             options.ElapsedTimeFormatSpecifier = "F3";
         }).Wait();
+        // Day_07 d = new();
+        // var x = d.Solve_1().AsTask().Result;
+        // Console.WriteLine(x);
+        // var y = d.Solve_2().AsTask().Result;
+        // Console.WriteLine(y);
     }
 }
--- Solutions/Day06/Day06.cs
+++ Solutions/Day06/Day06.cs
@@ -4,9 +4,36 @@ namespace Formula9.AdventOfCode.Solutions2023.Day06;
 
 public class Day_06 : AdventOfCodeProblem
 {
-    public Day_06() : base(2023, 06) { }
 
-    public override ValueTask<string> Solve_1() => new("Solution 1");
+    List<(long time, long distance)> TimeDistancePairs { get; init; }
+    (long time, long distance) FinalRound { get; init; }
 
-    public override ValueTask<string> Solve_2() => new("Solution 2");
+    public Day_06() : base(2023, 06) 
+    { 
+        var lines = Input.SplitByNewline().ToArray();
+        TimeDistancePairs = GetNumbersForLine(lines.First()).Zip(GetNumbersForLine(lines.Last())).ToList();
+        FinalRound = (long.Parse(string.Join(string.Empty, GetNumbersForLine(lines.First()))), long.Parse(string.Join(string.Empty, GetNumbersForLine(lines.Last()))));
+    }
+
+    private static IEnumerable<long> GetNumbersForLine(string s) =>s
+        .Split(":", StringSplitOptions.RemoveEmptyEntries)
+        .Last()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(long.Parse);
+
+    public static long CountSolutions((long time, long distance) pair)
+    {
+        long solutions = 0;
+        for (long timeSpentHoldingButton = 0; timeSpentHoldingButton < pair.time; timeSpentHoldingButton++)
+        {
+            // i = speed
+            long remainingTime = pair.time - timeSpentHoldingButton;
+            solutions += ((remainingTime * timeSpentHoldingButton) > pair.distance).ToInt();
+        }
+        return solutions;
+    }
+
+    public override ValueTask<string> Solve_1() => new(TimeDistancePairs.Select(CountSolutions).Aggregate((acc, el) => acc * el).ToString());
+
+    public override ValueTask<string> Solve_2() => new(CountSolutions(FinalRound).ToString());
 }
